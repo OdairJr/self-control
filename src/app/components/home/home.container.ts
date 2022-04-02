1082db97e69ef3b7ae7cf8a0d72508689ab7604f
Odair Junior <odair@hotmail.com>
2022-04-02 00:11:06 -0300

    Adicionando package-lock para o build

--- src/app/components/home/home.container.ts
+++ src/app/components/home/home.container.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { Bill } from 'src/app/models/bill.model';
 import { BillsService } from 'src/app/services/bills/bills.service';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
